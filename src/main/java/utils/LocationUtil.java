ce779205a85e6d8345e938257a73c65c0b7702fd
Nathan Kaufman <natecat4444@gmail.com>
2026-04-11 23:17:57 -0400

    Outlining location Util

---
+++ src/main/java/utils/LocationUtil.java
@@ -0,0 +1,14 @@
+package utils;
+
+import com.github.ocraft.s2client.protocol.spatial.Point2d;
+import java.util.Set;
+
+public class LocationUtil {
+    public Point2d getEnemyMain(){
+        return null;
+    }
+
+    public Set<Point2d> getOtherBases(){
+        return null;
+    }
+}
